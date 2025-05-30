e1752fd530b07485063939e340b5b140d78d8b9d
lorenz234 <lorenz_lehmann@yahoo.de>
2025-05-30 17:57:55 +0200

    Initial Commit

---
+++ FiatNest/FiatNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiatNestApp.swift
+//  FiatNest
+//
+//  Created by Lorenz Lehmann on 30.05.25.
+//
+
+import SwiftUI
+
+@main
+struct FiatNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FiatNestTests/FiatNestTests.swift
@@ -0,0 +1,17 @@
+//
+//  FiatNestTests.swift
+//  FiatNestTests
+//
+//  Created by Lorenz Lehmann on 30.05.25.
+//
+
+import Testing
+@testable import FiatNest
+
+struct FiatNestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
